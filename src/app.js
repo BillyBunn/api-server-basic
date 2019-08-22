2de1ff2dca4027be3fd7ccd953b4a979c0d8e9a5
Billy Bunn <williammbunn@gmail.com>
2019-08-22 13:21:22 -0700

    updates error handler

--- src/app.js
+++ src/app.js
@@ -11,19 +11,85 @@
 const express = require('express')
 const morgan = require('morgan')
 
+// Models
+const Categories = require('./models/categories')
+const categories = new Categories()
+
 // Prepare express app
 const app = express()
 
 // Application-level middleware
 /** HTTP request logger */
 app.use(morgan('dev'))
+app.use(express.json())
 
-// Routes
+// Routes --------------------------------------------------
 /** Static route to serve JSDocs */
 app.use('/docs', express.static('docs'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/error', (req, res, next) => next('error on purpose'))
+app.get('/error', (req, res, next) =>
+  next({
+    name: 'Test error',
+    message: 'This route causes a 500 error on purpose'
+  })
+)
+
+// Categories
+app.get('/categories', getCategories)
+app.post('/categories', postCategory)
+app.get('/categories/:id', getCategory)
+app.put('/categories/:id', putCategory)
+app.delete('/categories/:id', deleteCategory)
+
+function getCategories(req, res, next) {
+  // expects an array of object to be returned from the model
+  categories
+    .get()
+    .then(data => {
+      const output = {
+        count: data.length,
+        results: data
+      }
+      res.status(200).json(output)
+    })
+    .catch(next)
+}
+
+function getCategory(req, res, next) {
+  // expects an array with the one matching record from the model
+  categories
+    .get(req.params.id)
+    .then(result => res.status(200).json(result[0]))
+    .catch(next)
+}
+
+function postCategory(req, res, next) {
+  // expects the record that was just added to the database
+  categories
+    .post(req.body)
+    .then(result => {
+      // console.log({ result })
+      return res.status(200).json(result)
+    })
+    .catch(next)
+}
+
+function putCategory(req, res, next) {
+  // expects the record that was just updated in the database
+  categories
+    .put(req.params.id, req.body)
+    .then(result => res.status(200).json(result[0]))
+    .catch(next)
+}
+
+function deleteCategory(req, res, next) {
+  // Expects no return value (resource was deleted)
+  categories
+    .delete(req.params.id)
+    .then(result => res.status(200).json(result))
+    .catch(next)
+}
 
 app.use(notFound)
 app.use(errorHandler)
@@ -52,12 +118,15 @@ function notFound(req, res, next) {
  * @param {function} next - Express middleware function
  */
 function errorHandler(err, req, res, next) {
-  console.log('BOOYAH', typeof err)
-  console.log({ err })
-  let error = { error: err }
+  console.error('ERROR:', err)
+
+  const status = err.status || 500,
+    name = err.name || 'Unknown Error',
+    message = typeof err === 'string' ? err : err.message || 'An error occurred'
+
   res
-    .status(500)
-    .json(error)
+    .status(status)
+    .json({ status, name, message })
     .end()
 }
 
