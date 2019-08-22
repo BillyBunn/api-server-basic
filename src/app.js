5cbe4e7f063cbf0529e1abddff35c491802c6d8b
Billy Bunn <williammbunn@gmail.com>
2019-08-21 19:51:51 -0700

    adds error handler

--- src/app.js
+++ src/app.js
@@ -23,13 +23,13 @@ app.use(morgan('dev'))
 app.use('/docs', express.static('docs'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
-
+app.get('/error', (req, res, next) => next('error on purpose'))
 
 app.use(notFound)
+app.use(errorHandler)
 
 /**
- * Sends a JSON formatted 404 response
+ * Sends a JSON formatted "404 Not Found" response
  *
  * @param {object} req - Express HTTP request object
  * @param {object} res - Express HTTP response object
@@ -43,6 +43,23 @@ function notFound(req, res, next) {
     .end()
 }
 
+/**
+ * Sends a JSON formatted "500 Internal Server Error" response
+ *
+ * @param {(object|string)} err - Express server error message
+ * @param {object} req - Express HTTP request object
+ * @param {object} res - Express HTTP response object
+ * @param {function} next - Express middleware function
+ */
+function errorHandler(err, req, res, next) {
+  console.log('BOOYAH', typeof err)
+  console.log({ err })
+  let error = { error: err }
+  res
+    .status(500)
+    .json(error)
+    .end()
+}
 
 /**
  * Starts the server at specified port
