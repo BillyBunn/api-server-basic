84b794ff0f36afa3fd879ab70294741fcf0c3fb8
Billy Bunn <williammbunn@gmail.com>
2019-08-24 20:10:58 -0700

    refactors model middleware and adds model list route

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -1,23 +1,31 @@
 'use strict'
 
 /**
- * @ Model-finder middleware
+ * @file Data model utilities and middleware
  * @module middleware/model-finder
  */
 
-/**
- * Evaluates req.params.model (i.e. /api/v1/:model/) and returns an instance of the specified model.
- * Because node require is cached, the instance will only be created once, no matter how many times a model is called for.
- * In the event the model is not found, node will throw a "MODULE_NOT_FOUND" error which the error middleware in the server will pick up.
- * @param req {object} Express Request Object
- * @param res {object} Express Response Object
- * @param next {function} Express middleware next()
- */
+const { readdir, statSync, lstatSync } = require('fs')
+const { promisify } = require('util')
 
-function modelFinder(req, res, next) {
+const modelsFolder = `${__dirname}/../models`
+
+function attachModel(req, res, next) {
   let modelName = req.params.model.replace(/[^a-z0-9-_]/gi, '')
   req.model = require(`../models/${modelName}/${modelName}-model.js`)
   next()
 }
 
-module.exports = modelFinder
+function listModels() {
+  return promisify(readdir)(modelsFolder)
+    .then(contents =>
+      contents.filter(
+        item =>
+          lstatSync(`${modelsFolder}/${item}`).isDirectory() &&
+          statSync(`${modelsFolder}/${item}/${item}-model.js`)
+      )
+    )
+    .catch(console.error)
+}
+
+module.exports = { attachModel, listModels }
